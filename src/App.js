83ecefccaa4f3656d69f60bd9d484157718b4f07
Shahroo Khan Sahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-05-25 13:43:29 +0500

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+ import React, { useState } from 'react';
+ import "./App.css";
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-function App() {
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const handleTodoDelete = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a new todo"
+          value={newTodo}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <>
+        {todos.map((todo, index) => (
+          <h3 key={index}>
+            {todo}
+            <button onClick={() => handleTodoDelete(index)}>Delete</button>
+          </h3>
+        ))}
+      </>
     </div>
   );
-}
-
+};
 export default App;
